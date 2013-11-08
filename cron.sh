19c6d2681fdecc8be745e174e5d40775b2a45d07
isaacs <i@izs.me>
2013-11-08 12:52:38 -0800

    update previous day, to be safe

--- cron.sh
+++ cron.sh
@@ -5,9 +5,23 @@
 
 # exit on any errors
 set -e
-
-id=$1
-scp -i $id ubuntu@npm.m.ic.ht:hosting/servers/isaacs/db/log.1 .
+set -x
 
 dir=$(dirname $0)
-node $dir/upload.js log.1
+cd $dir
+
+# also do the previous day, just to be safe
+for n in '.1' '.2.gz'; do
+  for ip in $(dig +short isaacs.iriscouch.com | grep ^[0-9]); do
+    id=$1
+    scp -i $id isaacs@$ip:~ubuntu/hosting/servers/isaacs/db/log/'*'$n ./$ip.log$n
+
+    NODE_TLS_REJECT_UNAUTHORIZED=0 node upload.js $ip.log$n
+
+    d=$(date '+%Y-%m-%d')
+
+    if [ $n == '.1' ]; then
+      scp -i $id $ip.log npmlogs@nodesecurity.io:logs/$ip-$d.log || true
+    fi
+  done
+done
