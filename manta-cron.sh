4552ac717c26c4fef615b3960747791c71a97455
isaacs <i@izs.me>
2014-01-16 17:44:57 -0800

    use proper manta creds in cron.sh

--- manta-cron.sh
+++ manta-cron.sh
@@ -8,6 +8,8 @@ export MANTA_KEY_ID=55:5e:9a:bc:42:59:df:cb:ad:00:54:f6:59:53:20:83
 export MANTA_USER=npm
 export MANTA_URL=https://us-east.manta.joyent.com
 
+. ~node/manta.env
+
 id=$1
 cmd="ls ~ubuntu/hosting/servers/isaacs/db/log/*.1"
 d=$(date '+%Y-%m-%d')
